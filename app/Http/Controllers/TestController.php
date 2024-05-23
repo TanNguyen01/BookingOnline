f0b87d028603301daf8dfef7eb00d10999dac232
DESKTOP-C951187\HI <tienrider30@gmailcom>
2024-05-23 09:50:24 +0700

    refactor: demo Test

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\APIResponse;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    use APIResponse;
+
+    public function test(Request $request)
+    {
+        $data = ['1', '2', '3'];
+        $entry = ['first name', 'last name', 'email'];
+        return $this->responseSuccess('thêm thành công',
+            [
+                'data' => $data,
+                'entry' => $entry
+            ]);
+    }
+}
