f573ab77f8a4c8cb616cbed05de27155613f9960
TanNguyen01 <tannhph18714@fpt.edu.vn>
2024-06-07 09:13:01 +0700

    fix localization

---
+++ resources/lang/vi-VN/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /** Login */
+
+    "failed" => "Sai email hoặc mật khẩu .",
+    "success" => " Dang nhap thanh cong.",
+
+
+
+    /** Logout */
+    "logout_success" => "Dang xuat thanh cong.",
+];
