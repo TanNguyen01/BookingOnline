dcb3a1b8a4856794c63fd2c604ab90e81ac8cfac
TanNguyen01 <tannhph18714@fpt.edu.vn>
2024-06-07 09:15:52 +0700

    fix localization

---
+++ resources/lang/vi-VN/service.php
@@ -0,0 +1,11 @@
+<?php
+
+ return [
+     'list' => 'Xem thành công tat ca dich vu。',
+     'show' =>    'Xem dich vu thanh cong',
+     'created' => 'Dich vu đã được tạo thành công.',
+     'updated' => 'Dich vu đã được cập nhật thành công.',
+     'deleted' => 'Dich vu đã được xóa thành công.',
+     'not_found' => 'Không tìm thấy dich vu.',
+
+ ];
