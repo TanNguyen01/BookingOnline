911154876d703aa67e03cdd2de75f03dcbc86a09
TanNguyen01 <tannhph18714@fpt.edu.vn>
2024-06-13 09:30:23 +0700

    fix

---
+++ database/migrations/2024_06_11_154022_create_bases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bases', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('booking_id');
+            $table->string('email');
+            $table->string('name');
+            $table->date('date');
+            $table->string('phone');
+            $table->string('status'); // trạng thái của từng mục cụ thể
+            $table->text('note')->nullable();
+            $table->timestamps();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('base');
+    }
+};
