e9a51d4451632399b8e9845a0932bf589bcc8c0d
koko28011996 <oke.anhlathangbech@gmail.com>
2024-06-18 22:00:15 +0700

    fix

--- database/migrations/2013_create_store_information_table.php
+++ database/migrations/2013_create_store_information_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('store_informations');
+        Schema::dropIfExists('store_information');
     }
 };
